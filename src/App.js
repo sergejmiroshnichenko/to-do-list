922bdc85b7afca5980e1d45d9d91d20566f6d17b
Sergej Miroshnichenko <sergej.miroshnichenko.kr@gmail.com>
2022-05-31 13:27:02 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.module.scss';
+
+function App() {
+
+  return (
+        <div className="App">
+          <AddNote/>
+          <Modal />
+          <NoteContainer/>
+        </div>
+  );
+}
+
+export default App;
+
